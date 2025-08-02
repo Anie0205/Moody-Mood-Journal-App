011020c6343c51b672db03525cc7380f9d49aa0c
Anie0205 <ananya.verma0205@gmail.com>
2025-08-02 14:53:55 +0530

    Backend commit #3

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,8 +12,7 @@ exports.registerUser = async (req, res) => {
         const userExists = await User.findOne({email});
         if (userExists) return res.status(400).json({message: "User already Exists"});
 
-        const hashed = await bcrypt.hash(password, 10);
-        const user = await User.create({email, password: hashed});
+        const user = await User.create({email, password});
 
         res.status(201).json({
             _id: user._id,
